fe235e2ef6e2a4c9b33cbd31b0439edef30b9222
Alexander DL <alexanderdl@macbook-air-de-alexander.local>
2016-04-27 22:04:18 -0500

    Initial Commit

---
+++ testGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testGit
+//
+//  Created by Alexander DL on 27/04/16.
+//  Copyright © 2016 Alexander DL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGit
+//
+//  Created by Alexander DL on 27/04/16.
+//  Copyright © 2016 Alexander DL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
